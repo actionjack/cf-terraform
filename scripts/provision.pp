e01fd1b643dff284d2234a30bb171dce59c95b42
Jim Conner <jim.conner@digital.cabinet-office.gov.uk>
2015-11-18 11:58:23 +0000

    Fetch required binaries

--- scripts/provision.pp
+++ scripts/provision.pp
@@ -1,10 +1,16 @@
+$bosh_init_version='0.0.72'
+$bosh_init_url="https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-${bosh_init_version}-linux-amd64"
+
+$spiff_version='v1.0.7'
+$spiff_url="https://github.com/cloudfoundry-incubator/spiff/releases/download/${spiff_version}/spiff_linux_amd64.zip"
+
+$cf_cli_version='6.12.3'
+
 $packages = [
   'build-essential',
   'git',
   'zlibc',
   'zlib1g-dev',
-  'ruby',
-  'ruby-dev',
   'openssl',
   'libxslt1-dev',
   'libxml2-dev',
@@ -15,17 +21,67 @@ $packages = [
   'libsqlite3-dev',
   'sqlite3',
   'dstat',
-  'unzip',
-  'bundler',
   'jq',
 ]
+
+
+# Actions start from here.
 package { $packages:
   ensure => 'installed',
 }
-exec { 'bundle install':
-  logoutput => true,
-  cwd       => '/home/ubuntu/scripts',
-  path      => '/bin:/usr/bin:/usr/local/bin',
-  unless    => 'bundle check',
+
+class { 'ruby':
+  gems_version  => 'latest'
 }
-notify {'i am running':}
+
+bundler::install { '/home/ubuntu/scripts':
+  user       => 'ubuntu',
+  group      => 'users',
+  deployment => true,
+  without    => 'development test doc',
+}
+
+# Install required binaries
+notify {'Installing binaries: bosh-init':}
+
+include wget
+wget::fetch { 'bosh-init':
+  source    => $bosh_init_url,
+  destination => '/usr/local/bin/bosh-init',
+}
+
+file { '/usr/local/bin/bosh-init':
+  path      => '/usr/local/bin/bosh-init',
+  mode      => '0755',
+}
+
+download_uncompress {'spiff':
+   distribution_name  => $spiff_url,
+   dest_folder   => '/usr/local/bin/',
+   creates       => "/usr/local/bin/spiff",
+   uncompress    => 'zip',
+}
+
+wget::fetch { 'spiff':
+  source    => $spiff_url,
+  destination => '/tmp/spiff.zip',
+}
+
+
+file { '/usr/local/bin/spiff':
+  path      => '/usr/local/bin/spiff',
+  mode      => '0755',
+}
+
+
+wget::fetch { 'cf-client':
+  source      => "https://cli.run.pivotal.io/stable?release=debian64&version=${cf_cli_version}&source=github-rel",
+  destination => "/tmp/cf-cli_${cf_cli_cersion}_amd64.deb",
+}
+
+package { 'cf-cli' :
+  provider    => dpkg,
+  ensure      => latest,
+  source      => "/tmp/cf-cli_${cf_cli_cersion}_amd64.deb",
+}
+
--- scripts/puppet_provision.sh
+++ scripts/puppet_provision.sh
@@ -7,5 +7,10 @@ wget https://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
 sudo dpkg -i puppetlabs-release-pc1-trusty.deb
 sudo apt-get update
 sudo apt-get install puppet-agent -y
+
+sudo /opt/puppetlabs/puppet/bin/puppet module install maestrodev-wget
+sudo /opt/puppetlabs/puppet/bin/puppet module install puppetlabs-ruby
+sudo /opt/puppetlabs/puppet/bin/puppet module install ploperations-bundler
+sudo /opt/puppetlabs/puppet/bin/puppet module install dsestero-download_uncompress
 sudo /opt/puppetlabs/puppet/bin/puppet apply scripts/provision.pp
 
